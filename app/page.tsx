57d0a50f07e5b2e371a245505902f5fb754dc825
Spencer Anderson <sanderson28801@gmail.com>
2025-12-20 18:39:35 -0500

    got showcase cards working

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
-
+import Intro from "@/components/sections/Intro";
+import Experience from "@/components/sections/Experience";
+import Projects from "@/components/sections/Projects";
 
 export default function Portfolio() {
   return (
-    <main className="bg-background min-h-screen flex flex-col items-center justify-center p-4">
-      <div className="max-w-3xl w-full">
-        <h1 className="text-header-size text-header">Hi! I'm Spencer</h1>
-        
-
-      </div>
-    </main>);
+    <>
+      <Intro />
+      <Experience />
+      <Projects />
+    </>
+  );
 }
